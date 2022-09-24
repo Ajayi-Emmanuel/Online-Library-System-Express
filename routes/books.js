72bfd8efb5d09d14224da2549af0d8b89a7c505f
*Ajayi <*emmanueltemitayo056@gmail.com*>
2022-09-24 09:35:58 +0100

    completed the code for all routes

--- routes/books.js
+++ routes/books.js
@@ -5,8 +5,6 @@ const path = require("path")
 const bookfilepath = path.join("c:/Users/USER/Documents/ALT School/BACK-END ENGINEERING/Js/Online-Library-System-Express", "db", "books.json")
 const bookloanfilepath = path.join("c:/Users/USER/Documents/ALT School/BACK-END ENGINEERING/Js/Online-Library-System-Express", "db", "loan.json")
 
-bookLoanDb = []
-
 const bookRoute = express.Router();
 
 bookRoute.get("/", (req, res) => {
@@ -28,7 +26,7 @@ bookRoute.post("/addbook", (req, res) => {
     })
 })
 
-bookRoute.post("/delete/:id", (req, res) => {
+bookRoute.delete("/delete/:id", (req, res) => {
     const bookid = JSON.parse(req.params.id)
     fs.readFile(bookfilepath, "utf8", (err, books) => {
         if (err) {
@@ -46,7 +44,7 @@ bookRoute.post("/delete/:id", (req, res) => {
         fs.writeFile(bookfilepath, JSON.stringify(booksObj), (err) => {
             if (err) {
                 res.status(500).json({
-                    message: 'Internal Server Error. Could not save book to database.'
+                    message: 'Internal Server Error. Could not delete book from database'
                 });
             }
             res.status(200).send("Deletion successful!");
@@ -65,13 +63,7 @@ bookRoute.post("/loan", (req, res) => {
 
         const booksObj = JSON.parse(books)
         const bookdetails = booksObj.find(book => book.title === booktitle)
-        const bookId = bookdetails.id
-        const bookIndex = booksObj.findIndex(book => book.id === bookId)
-
-        if (bookIndex === -1) {
-            res.status(404).send("Book with the specified id not found!")
-            return
-        }
+        if(bookdetails === undefined) return res.status(404).send("Incorrect name entered")
         
         bookLoanDb.push(bookdetails);
         fs.writeFile(bookloanfilepath, JSON.stringify(bookLoanDb), (err) => {
@@ -83,11 +75,14 @@ bookRoute.post("/loan", (req, res) => {
             res.status(200)
         });
 
-        booksObj.splice(bookIndex, 1)             
+        const bookId = bookdetails.id
+        const bookIndex = booksObj.findIndex(book => book.id === bookId)
+        booksObj.splice(bookIndex, 1)   
+
         fs.writeFile(bookfilepath, JSON.stringify(booksObj), (err) => {
             if (err) {
                 res.status(500).json({
-                    message: 'Internal Server Error. Could not save book to database.'
+                    message: 'Internal Server Error. Could not delete book from Master database.'
                 });
             }
             res.status(200).send("Loan Successful")
@@ -96,10 +91,70 @@ bookRoute.post("/loan", (req, res) => {
     })
 })
 
+bookRoute.post("/returnbook", (req, res) =>{
+    const booktitle = req.body.title;
 
-// bookRoute.get("/allbooks", (req, res) => {
-//     res.json(books)
-// })
+    fs.readFile(bookloanfilepath, "utf8", (err, books) => {
+        if (err) {
+            res.status(400).send("An error occured")
+        }
+        const booksObj = JSON.parse(books)
+        const bookdetails = booksObj.find(book => book.title === booktitle)
+        if(bookdetails === undefined) return res.status(404).send("Incorrect name entered")
+
+        booksDb.push(bookdetails)
+        fs.writeFile(bookfilepath, JSON.stringify(booksDb), (err) => {
+            if (err) {
+                res.status(500).send({
+                    message: 'Internal Server Error. Could not return book to book database.'
+                });
+            }
+        });
+
+        bookId = bookdetails.id
+        const bookIndex = booksObj.findIndex(book => book.id === bookId)
+        booksObj.splice(bookIndex, 1)
+        
+        fs.writeFile(bookloanfilepath, JSON.stringify(booksObj), (err) => {
+            if (err) {
+                res.status(500).send({
+                    message: 'Internal Server Error. Could not remove book from loaned Book database.'
+                });
+            }   
+        });
+        res.status(200).send("Book has been Returned")
+    })
+})
+
+bookRoute.put("/update", (req, res) => {
+    const bookupdate = req.body
+    const bookupdateId = bookupdate.id
+
+    fs.readFile(bookfilepath, "utf8", (err, books) => {
+        if (err) {
+            res.status(400).send("An error occured")
+        }
+
+        const booksObj = JSON.parse(books)
+        const bookIndex = booksObj.findIndex(book => book.id === bookupdateId)
+
+        if (bookIndex === -1) return res.status(404).send("Book with the specified id not found!")
+
+        const updatedBook = { ...booksObj[bookIndex], ...bookupdate }
+        booksObj[bookIndex] = updatedBook
+
+        fs.writeFile(bookfilepath, JSON.stringify(booksObj), (err) => {
+            if (err) {
+                res.status(500).send({
+                    message: 'Internal Server Error. Could update book.'
+                });
+            }
+            res.status(200).send("Update successful!");
+        });
+
+    })
+})
+
 
 booksDb = JSON.parse(fs.readFileSync(bookfilepath, 'utf8'));
 bookLoanDb = JSON.parse(fs.readFileSync(bookloanfilepath, 'utf8'));
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,9 @@
 const express = require("express")
+const fs = require("fs")
+const path = require("path")
 
+const userfilepath = path.join("c:/Users/USER/Documents/ALT School/BACK-END ENGINEERING/Js/Online-Library-System-Express", "db", "users.json")
 const userRoute = express.Router();
-
-const users = [
-    {
-        "name": "Ajayi Emmanuel",
-        "username": "Emman34@",
-        "email": "emmanueltester2@gmail.com",
-        "phonenumber": "080324435121",
-        "address": "17, elepo street banada anthony",
-        "password": "tester23?"
-    }
-]
 
 userRoute.get("/", (req, res) => {
     res.json("Welcome to the user Route")
@@ -19,9 +11,23 @@ userRoute.get("/", (req, res) => {
 
 userRoute.post("/adduser", (req, res) => {
     const newUser = req.body
-    users.push(newUser);
-    res.send(users)
+    usersDb.push(newUser);
+    fs.writeFile(userfilepath, JSON.stringify(usersDb), (err) => {
+        if (err) return res.status(400).send(err)
+    })
+    res.status(200).send("New User Added")
 })
 
+userRoute.get("/allusers", (req,res) => {
+    fs.readFile(userfilepath, "utf-8", (err, users) => {
+        if (err){
+            res.status(400).json({
+                error: "Server Error!"
+            })
+        }
+        res.status(200).send(users)
+    })
+})
 
+usersDb = JSON.parse(fs.readFileSync(userfilepath, 'utf8'));
 module.exports = userRoute
